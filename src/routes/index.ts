4f89ab6bd7098414002661030253ad1b573ec815
Hafidz Athaya <mhafidza528@gmail.com>
2024-07-21 17:00:52 +0800

    extract auth route into separate file and register it with router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,3 +1,17 @@
-import express from 'express';
+import express, { Router } from 'express';
+import authRoute from './auth.routes';
 
 export const router = express.Router();
+
+const defaultRoute: { path: string; route: Router }[] = [
+  {
+    path: '/auth',
+    route: authRoute
+  }
+];
+
+defaultRoute.forEach((route) => {
+  router.use(route.path, route.route);
+});
+
+export default router;
