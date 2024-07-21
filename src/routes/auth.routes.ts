495040cd3adabb14c540355ee80fcfbe2c6fd744
Hafidz Athaya <mhafidza528@gmail.com>
2024-07-21 17:05:20 +0800

    move the handler to controller

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,21 +1,11 @@
+import * as controller from '../controllers/auth.controller';
 import express from 'express';
 
 const authRoute = express.Router();
 
-authRoute.post('/register', (req, res) => {
-  res.send('Register');
-});
-
-authRoute.post('/login', (req, res) => {
-  res.send('Login');
-});
-
-authRoute.post('/logout', (req, res) => {
-  res.send('Logout');
-});
-
-authRoute.post('/refresh-token', (req, res) => {
-  res.send('Refresh Token');
-});
+authRoute.post('/register', controller.register);
+authRoute.post('/login', controller.login);
+authRoute.post('/logout', controller.logout);
+authRoute.post('/refresh-token', controller.refreshToken);
 
 export default authRoute;
