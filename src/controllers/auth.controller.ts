420abfd728d2ebb51ea8b25ebbade4efec877264
Hafidz Athaya <mhafidza528@gmail.com>
2024-07-21 17:22:40 +0800

    controller for auth routes

---
+++ src/controllers/auth.controller.ts
@@ -0,0 +1,33 @@
+import { Request, Response, NextFunction } from 'express';
+
+export const register = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    res.send('Register');
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const login = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    res.send('Login');
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const logout = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    res.send('logout');
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const refreshToken = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    res.send('Refresh Token');
+  } catch (error) {
+    next(error);
+  }
+};
