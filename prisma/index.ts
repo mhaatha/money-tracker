2d1e7bf3537761536a0b4a60d7f75d40899594bb
Hafidz Athaya <mhafidza528@gmail.com>
2024-07-21 14:48:22 +0800

    prisma logging event based combo with winston logger

--- prisma/index.ts
+++ prisma/index.ts
@@ -1,3 +1,39 @@
+import { logger } from '../src/utils/logger';
 import { PrismaClient } from '@prisma/client';
 
-export const prisma = new PrismaClient();
+export const prisma = new PrismaClient({
+  log: [
+    {
+      emit: 'event',
+      level: 'query'
+    },
+    {
+      emit: 'event',
+      level: 'info'
+    },
+    {
+      emit: 'event',
+      level: 'warn'
+    },
+    {
+      emit: 'event',
+      level: 'error'
+    }
+  ]
+});
+
+prisma.$on('query', (e) => {
+  logger.info(`Query: ${e.query}`);
+});
+
+prisma.$on('info', (e) => {
+  logger.info(`Info: ${e.message}`);
+});
+
+prisma.$on('warn', (e) => {
+  logger.warn(`Warning: ${e.message}`);
+});
+
+prisma.$on('error', (e) => {
+  logger.error(`Error: ${e.message}`);
+});
