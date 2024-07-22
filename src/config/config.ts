dcfb0723919843d8d1646fd17cb13285a054fc4b
Hafidz Athaya <mhafidza528@gmail.com>
2024-07-22 15:07:28 +0800

    add database url envi

--- src/config/config.ts
+++ src/config/config.ts
@@ -7,6 +7,9 @@ export default {
   env: process.env.NODE_ENV,
   port: 3000,
   logLevel: process.env.LOG_LEVEL,
+  database: {
+    url: process.env.DATABASE_URL
+  },
   jwt: {
     secret: process.env.JWT_SECRET,
     accessExpirationMinutes: process.env.JWT_ACCESS_EXPIRATION_MINUTES,
