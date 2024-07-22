29154f38dd0dcf4fc3e0cdd272b8e4f3f34d114b
Hafidz Athaya <mhafidza528@gmail.com>
2024-07-22 15:06:48 +0800

    fix path join

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,10 +1,15 @@
 import dotenv from 'dotenv';
 import path from 'path';
 
-dotenv.config({ path: path.join(__dirname, '../../.env') });
+dotenv.config({ path: path.join(__dirname, '../../../.env') });
 
 export default {
   env: process.env.NODE_ENV,
   port: 3000,
-  log_level: process.env.LOG_LEVEL
+  logLevel: process.env.LOG_LEVEL,
+  jwt: {
+    secret: process.env.JWT_SECRET,
+    accessExpirationMinutes: process.env.JWT_ACCESS_EXPIRATION_MINUTES,
+    refreshExpirationDays: process.env.JWT_REFRESH_EXPIRATION_DAYS
+  }
 };
