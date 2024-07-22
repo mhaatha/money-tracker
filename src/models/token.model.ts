63db41a908dedbf5f947e606753a2999ce4d599e
Hafidz Athaya <mhafidza528@gmail.com>
2024-07-22 15:15:15 +0800

    token schema model

---
+++ src/models/token.model.ts
@@ -0,0 +1,22 @@
+export enum TokenTypes {
+  ACCESS = 'access',
+  REFRESH = 'refresh'
+}
+
+export interface Payload {
+  sub: string;
+  iat: number;
+  exp: number;
+  type: string;
+}
+
+export interface TokenResponse {
+  access: {
+    token: string;
+    expired: Date;
+  };
+  refresh: {
+    token: string;
+    expired: Date;
+  };
+}
