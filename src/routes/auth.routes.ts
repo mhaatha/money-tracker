98063e8ad93d2e8da56ca215d5a8cbbae1388ad9
Hafidz Athaya <mhafidza528@gmail.com>
2024-07-23 10:39:01 +0800

    implement auth middleware

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,11 +1,12 @@
 import * as controller from '../controllers/auth.controller';
 import express from 'express';
+import { auth } from '../middlewares/auth.middleware';
 
 const authRoute = express.Router();
 
 authRoute.post('/register', controller.register);
 authRoute.post('/login', controller.login);
-authRoute.post('/logout', controller.logout);
-authRoute.post('/refresh-token', controller.refreshToken);
+authRoute.post('/logout', auth, controller.logout);
+authRoute.post('/refresh-token', auth, controller.refreshToken);
 
 export default authRoute;
