6f55135d38ecaabd7e83037e088614b6f86fbc1b
Hafidz Athaya <mhafidza528@gmail.com>
2024-07-23 14:06:40 +0800

    user schema model

---
+++ src/models/user.model.ts
@@ -0,0 +1,33 @@
+import { User } from '@prisma/client';
+
+export interface GetUserResponse {
+  id: string;
+  username: string;
+  email: string;
+  first_name: string | null;
+  last_name: string | null;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export interface GetUserBalanceResponse {
+  balance: number;
+}
+
+export const toGetUserResponse = (data: User): GetUserResponse => {
+  return {
+    id: data.id,
+    username: data.username,
+    email: data.email,
+    first_name: data.first_name,
+    last_name: data.last_name,
+    createdAt: data.createdAt,
+    updatedAt: data.updatedAt
+  };
+};
+
+export const toGetUserBalanceResponse = (data: User): GetUserBalanceResponse => {
+  return {
+    balance: data.total_balance
+  };
+};
