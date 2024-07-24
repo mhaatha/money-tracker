b2b67f74efd5364f7d97fa2f5173f7c525c899ee
Hafidz Athaya <mhafidza528@gmail.com>
2024-07-24 17:28:45 +0800

    add zod validation for update mutation ledger

--- src/validations/mutationLedger.validation.ts
+++ src/validations/mutationLedger.validation.ts
@@ -19,3 +19,29 @@ export const mutationLedgerBodyRequest: ZodType = z
     amount: z.number().positive({ message: 'Amount must be a positive number' })
   })
   .strict();
+
+export const updateMutation: ZodType = z
+  .object({
+    type: z
+      .enum(['Income', 'Expense'], {
+        message: 'Type must be Income or Expense'
+      })
+      .optional(),
+    user_id: z
+      .string()
+      .regex(/^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/, {
+        message: 'User_id is not a valid uuid'
+      })
+      .optional(),
+    category_id: z
+      .string()
+      .regex(/^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/, {
+        message: 'Category_id is not a valid uuid'
+      })
+      .optional(),
+    amount: z
+      .number()
+      .positive({ message: 'Amount must be a positive number' })
+      .optional()
+  })
+  .strict();
