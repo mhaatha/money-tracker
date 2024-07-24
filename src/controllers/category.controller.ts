abc34f6a59c9deffc5b004446ad680258cac9a64
Hafidz Athaya <mhafidza528@gmail.com>
2024-07-24 14:10:53 +0800

    controllers for category route

---
+++ src/controllers/category.controller.ts
@@ -0,0 +1,64 @@
+import { Payload } from '../models/token.model';
+import { StatusCodes } from 'http-status-codes';
+import { UserRequest } from '../types/user.type';
+import { Response, NextFunction } from 'express';
+import { RequestBody, ResponseBody } from '../models/category.model';
+import {
+  createCategory,
+  deleteCategory,
+  getCategories,
+  updateCategory
+} from '../services/category.service';
+
+export const create = async (req: UserRequest, res: Response, next: NextFunction) => {
+  try {
+    const data: RequestBody = req.body;
+    const response: ResponseBody = await createCategory(data);
+
+    res.status(StatusCodes.CREATED).json({
+      data: response
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const get = async (req: UserRequest, res: Response, next: NextFunction) => {
+  try {
+    const user: Payload = req.user as Payload;
+    const response: ResponseBody[] = await getCategories(user.sub);
+
+    res.status(StatusCodes.OK).json({
+      data: response
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const update = async (req: UserRequest, res: Response, next: NextFunction) => {
+  try {
+    const data: RequestBody = req.body;
+    const categoryId: string = req.params.categoryId;
+    const response: ResponseBody = await updateCategory(data, categoryId);
+
+    res.status(StatusCodes.OK).json({
+      data: response
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleted = async (req: UserRequest, res: Response, next: NextFunction) => {
+  try {
+    const categoryId: string = req.params.categoryId;
+    await deleteCategory(categoryId);
+
+    res.status(StatusCodes.OK).json({
+      data: null
+    });
+  } catch (error) {
+    next(error);
+  }
+};
