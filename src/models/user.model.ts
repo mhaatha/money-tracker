2da3e1ac5ca71b90d88ee74f7636cdf5a23a9e2f
Hafidz Athaya <mhafidza528@gmail.com>
2024-07-24 13:18:29 +0800

    update user model schema

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -31,3 +31,31 @@ export const toGetUserBalanceResponse = (data: User): GetUserBalanceResponse => 
     balance: data.total_balance
   };
 };
+
+export interface UpdateBodyRequest {
+  username: string | null;
+  email: string | null;
+  password: string | null;
+  first_name: string | null;
+  last_name: string | null;
+}
+
+export interface UpdateBodyResponse {
+  username: string;
+  email: string | null;
+  first_name: string | null;
+  last_name: string | null;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export const toUpdateUserResponse = (data: User): UpdateBodyResponse => {
+  return {
+    username: data.username,
+    email: data.email,
+    first_name: data.first_name,
+    last_name: data.last_name,
+    createdAt: data.createdAt,
+    updatedAt: data.updatedAt
+  };
+};
