cc79b2910553ff285e8a472031e381e34e60fc08
Hafidz Athaya <mhafidza528@gmail.com>
2024-07-24 14:09:59 +0800

    category services

---
+++ src/services/category.service.ts
@@ -0,0 +1,85 @@
+import { prisma } from '../../prisma';
+import { validate } from '../validations/validation';
+import { StatusCodes } from 'http-status-codes';
+import { ResponseError } from '../utils/response-error';
+import { categoryBodyRequest } from '../validations/category.validation';
+import { RequestBody, ResponseBody } from '../models/category.model';
+
+export const createCategory = async (data: RequestBody): Promise<ResponseBody> => {
+  const createData: RequestBody = validate(categoryBodyRequest, data);
+
+  // CREATE CATEGORY
+  const category: ResponseBody = await prisma.category.create({
+    data: {
+      name: createData.name
+    }
+  });
+
+  return category;
+};
+
+export const getCategories = async (userId: string): Promise<ResponseBody[]> => {
+  const categories: ResponseBody[] = await prisma.category.findMany({
+    where: {
+      mutationLedgers: {
+        some: {
+          user_id: userId
+        }
+      }
+    }
+  });
+
+  return categories;
+};
+
+export const updateCategory = async (
+  data: RequestBody,
+  categoryId: string
+): Promise<ResponseBody> => {
+  const updateData: RequestBody = validate(categoryBodyRequest, data);
+
+  // VALIDATION: IS CATEGORY EXISTS
+  const category: ResponseBody | null = await prisma.category.findUnique({
+    where: {
+      id: categoryId
+    }
+  });
+  if (!category) {
+    throw new ResponseError(StatusCodes.NOT_FOUND, 'Category not found', {
+      path: 'category_id'
+    });
+  }
+
+  // UPDATE CATEGORY
+  const categoryUpdated: ResponseBody = await prisma.category.update({
+    where: {
+      id: categoryId
+    },
+    data: {
+      name: updateData.name
+    }
+  });
+
+  return categoryUpdated;
+};
+
+export const deleteCategory = async (categoryId: string): Promise<void> => {
+  // VALIDATION: IS CATEGORY EXISTS
+  const category: ResponseBody | null = await prisma.category.findUnique({
+    where: {
+      id: categoryId
+    }
+  });
+  if (!category) {
+    throw new ResponseError(StatusCodes.NOT_FOUND, 'Category not found', {
+      path: 'category_id'
+    });
+  }
+
+  // DELETE CATEGORY
+  await prisma.category.delete({
+    where: {
+      id: categoryId
+    }
+  });
+};
